264d3fdaf4376191f4f4ffffc4890568d416b8ee
jeewan rana <jeewanrana@jeewans-mbp.lan>
2024-10-17 10:51:26 -0700

    remove api key

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ""
 LLM_MODEL_NAME = "gpt-4-0125-preview"
 USER = "root"
 PASSWORD = "test"
